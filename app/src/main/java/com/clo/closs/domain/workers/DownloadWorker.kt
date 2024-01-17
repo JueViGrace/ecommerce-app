639f3c8bc6876ba8befad45b97760a8e483b85e3
Daniel Zabala <danielzabalanava@gmail.com>
2024-01-17 12:41:21 -0400

    work manager 2

--- app/src/main/java/com/clo/closs/domain/workers/DownloadWorker.kt
+++ app/src/main/java/com/clo/closs/domain/workers/DownloadWorker.kt
@@ -3,9 +3,13 @@ package com.clo.closs.domain.workers
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.clo.closs.common.Resource
+import com.clo.closs.common.log
+import com.clo.closs.domain.model.Product
 import com.clo.closs.domain.repository.MainRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.catch
 
 class DownloadWorker @AssistedInject constructor(
     private val repository: MainRepository,
@@ -13,6 +17,37 @@ class DownloadWorker @AssistedInject constructor(
     @Assisted workerParameters: WorkerParameters
 ) : CoroutineWorker(context, workerParameters) {
     override suspend fun doWork(): Result {
-        TODO("Not yet implemented")
+        return if (repository.getUserSession() == true) {
+            downLoad()
+        } else {
+            onStopped()
+            Result.failure()
+        }
+    }
+
+    private suspend fun downLoad(): Result {
+        var result = false
+
+        repository.getProductsFromApi().catch { err: Throwable ->
+            err.log("Product worker error")
+        }
+            .collect { resource: Resource<List<Product>> ->
+                when (resource) {
+                    is Resource.Error -> result = false
+                    is Resource.Loading -> result = false
+                    is Resource.Success -> {
+                        if (resource.data != null) {
+                            repository.upsertProduct(resource.data)
+                            result = true
+                        }
+                    }
+                }
+            }
+
+        return if (result) {
+            Result.success()
+        } else {
+            Result.retry()
+        }
     }
 }
